eafd0cbea629d2f779fa228db78c27837206f546
Nihal Kishore <nihal301298@gmail.com>
2023-05-26 17:04:38 +0530

    Levevl 4 Auth cookies & session

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 =require("md5");
+// const md5 =require("md5");
+// const bcrypt =require("bcrypt");
+// const saltRounds = 5;
+
+const session =require("express-session");
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
+
 
 const app = express();
 
@@ -15,6 +22,15 @@ app.use(
     extended: true,
   })
 );
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 //mongoose connection
 mongoose
@@ -30,15 +46,26 @@ mongoose
     console.error("Failed to connect to MongoDB:", error);
   });
 
+
+
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 // const secret = "Thisisourlittlesecret.";
 // userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -50,42 +77,56 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/secrets",(req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+})
+
+app.get("/logout",(req,res)=>{
+    // res.logout();
+    res.redirect("/");
+})
+
 //Register INFO
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser
-    .save()
-    .then((userData) => {
-      // Handle successful save
-      console.log("Instance saved:", userData);
-      res.render("secrets");
+    User.register({username:req.body.username},req.body.password,(err,user)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secrets');
+
+            })
+        }
     })
-    .catch((error) => {
-      // Handle error
-      console.error("Save error:", error);
-    });
+
+  
 });
 
 //LogIN INFO
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({ email: username })
-    .then((foundUser) => {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          console.log(password);
-          res.render("secrets");
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
     });
-});
+    req.login(user,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secrets');
+
+            })
+
+        }
+    })
+
+})
+
 
 app.listen(3000, () => {
   console.log("Server is started on port 3000");
