54c22f40d5ffd8937c583d26e831200cb3c545a8
Nihal Kishore <nihal301298@gmail.com>
2023-05-26 12:44:20 +0530

    first commit Atuh L1

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+const bodyParser = require("body-parser");
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+
+//mongoose connection
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true ,useUnifiedTopology: true})
+.then(() => {
+    console.log('Connected to MongoDB');
+    // Continue with the code below
+  })
+  .catch(error => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+
+  const userSchema = {
+    email:String,
+    password:String
+  };
+
+  const User = new mongoose.model("User",userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+//Register INFO 
+app.post("/register",(req,res)=>{
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save()
+  .then(userData => {
+    // Handle successful save
+    console.log('Instance saved:', userData);
+    res.render("secrets")
+  })
+  .catch(error => {
+    // Handle error
+    console.error('Save error:', error);
+  });
+
+
+});
+
+//LogIN INFO
+app.post("/login",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ email: username })
+  .then(foundUser => {
+    if (foundUser) {
+      if (foundUser.password === password) {
+        console.log(password)
+        res.render("secrets");
+      }
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server is started on port 3000")});
